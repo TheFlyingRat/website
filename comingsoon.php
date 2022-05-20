5f6fae75d7bfcf47ab7359227f1c6badedd13387
TheFlyingRat <joeyspp50@gmail.com>
2022-05-20 21:06:25 +1000

    new file:   .htaccess 	new file:   LICENSE.txt 	new file:   assets/img/corner_left_wireframe.png 	new file:   assets/js/home.js 	new file:   comingsoon.php 	new file:   errmsgs.txt 	new file:   index.min.html 	new file:   package-lock.json 	new file:   package.json 	new file:   pre/index.html

---
+++ comingsoon.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+  <!-- /*! index.php (home) 1.5.3 too many updates. | MIT -->
+  <head>
+    <title>Coming Soon - TheFlyingRat</title>
+  
+    <!-- Content type declaration -->
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+  
+    <!-- Meta tags -->
+    <meta name="name" content="Coming Soon - TheFlyingRat">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="application-name" content="Coming Soon - TheFlyingRat">
+    <meta name="author" content="/humans.txt">    
+    
+    <!-- Open Graph meta tags -->
+    <meta property="og:title" content="Coming Soon - TheFlyingRat">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="https://theflyingrat.com">
+    <meta property="og:image" content="/logo512x512.png">
+  
+    <!-- Link tags-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&family=Poppins&family=Raleway:wght@300;400;600&family=Roboto:wght@300&display=swap" rel="stylesheet">    <link rel="manifest" href="/site.webmanifest">
+    <link rel="author" href="/humans.txt">
+    <link rel="apple-touch-icon" href="/logo512x512.png">
+    <link rel="icon" type="image/png" href="/logo128x128.png">
+  
+    <!-- Stylesheets -->
+    <link rel="stylesheet" href="/assets/css/normalize.min.css">
+    <link rel="stylesheet" href="/assets/css/skeleton.min.css">
+    <link rel="stylesheet" href="/assets/css/main.min.css">
+
+
+    <style>
+      body {
+        height: 100vh;
+        margin: 0;
+        padding: 0;   
+      }
+      .centerMe {
+        display: flex;
+        flex-direction: column;
+        height: 100%;
+        width: 100%;
+        justify-content: center;
+        align-items: center;
+      }
+    </style>
+  </head>
+  
+  <body id="body" style="background-color: #222" class="bodyDark"> <!-- Default to dark theme <3 -->
+    <div id="dropShadow"></div> 
+    <!-- this literally does nothing.
+         just prevents a js error from 
+         coming up that im too lazy to fix.
+         #Programmer -->
+      <section style="height: 100%;">
+        <h1 class="centerMe">Coming Soon</h1> 
+    </section>
+
+    
+    <script src="/assets/js/vendor/modernizr-3.11.2.min.js"></script>
+    <script src="/assets/js/plugins.min.js"></script>
+    <script src="/assets/js/main.min.js"></script>
+    <script src="/assets/js/functions.min.js"></script>
+
+    <script src="/assets/js/home.js"></script>
+    
+
+  </body>
+
+
+<!-- End Document
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+</html>
